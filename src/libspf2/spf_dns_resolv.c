8bdce2df720028038e931e5a04e3d6ed6a8c883b
ddave1 <david_seares@symantec.com>
2012-02-14 10:52:39 +0000

    libspf now handles SPF RRs in linux (libresolv)

--- src/libspf2/spf_dns_resolv.c
+++ src/libspf2/spf_dns_resolv.c
@@ -179,14 +179,15 @@ SPF_dns_resolv_debug(SPF_dns_server_t *spf_dns_server, ns_rr rr,
 				SPF_debugf("MX: %d %s", prio, name_buf);
 			break;
 
+		case ns_t_spf:
 		case ns_t_txt:
 			if (rdlen < 1) {
-				SPF_debugf("TXT: rdlen too short: %lu", (unsigned long)rdlen);
+				SPF_debugf(ns_rr_type(rr) == ns_t_txt ? "TXT" : "SPF" ": rdlen too short: %lu", (unsigned long)rdlen);
 				break;
 			}
 			/* XXX I think this is wrong/unsafe. Shevek. */
 			/* XXX doesn't parse the different TXT "sections" */
-			SPF_debugf("TXT: (%lu) \"%.*s\"",
+			SPF_debugf(ns_rr_type(rr) == ns_t_txt ? "TXT" : "SPF" ": (%lu) \"%.*s\"",
 					(unsigned long)rdlen, (int)rdlen - 1, rdata + 1);
 			break;
 
@@ -510,6 +511,7 @@ SPF_dns_resolv_lookup(SPF_dns_server_t *spf_dns_server,
 					cnt++;
 					break;
 
+				case ns_t_spf:
 				case ns_t_txt:
 					if (rdlen > 1) {
 						u_char *src, *dst;
