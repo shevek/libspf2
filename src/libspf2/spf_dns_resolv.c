7a64581d3493cb1ae53701d56f51295770ae0854
Andre Nathan <andre@digirati.com.br>
2012-07-17 13:20:08 -0300

    Let the code compile

--- src/libspf2/spf_dns_resolv.c
+++ src/libspf2/spf_dns_resolv.c
@@ -182,12 +182,12 @@ SPF_dns_resolv_debug(SPF_dns_server_t *spf_dns_server, ns_rr rr,
         case ns_t_spf:
 		case ns_t_txt:
 			if (rdlen < 1) {
-				SPF_debugf((ns_rr_type(rr) == ns_t_txt ? "TXT" : "SPF") ": rdlen too short: %lu", (unsigned long)rdlen);
+				SPF_debugf(ns_rr_type(rr) == ns_t_txt ? "TXT" : "SPF" ": rdlen too short: %lu", (unsigned long)rdlen);
 				break;
 			}
 			/* XXX I think this is wrong/unsafe. Shevek. */
 			/* XXX doesn't parse the different TXT "sections" */
-			SPF_debugf((ns_rr_type(rr) == ns_t_txt ? "TXT" : "SPF") ": (%lu) \"%.*s\"",
+			SPF_debugf(ns_rr_type(rr) == ns_t_txt ? "TXT" : "SPF" ": (%lu) \"%.*s\"",
 					(unsigned long)rdlen, (int)rdlen - 1, rdata + 1);
 			break;
 
