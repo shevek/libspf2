a97abaa5164eeaa5150bc3361eaaa038a2b56f07
Andre Nathan <andre@digirati.com.br>
2012-10-19 11:04:27 -0300

    Always retry after errors on SPF record lookup
    This is meant as a protection against old/buggy/crappy DNS servers
    which return errors when they receive SPF record queries.

--- src/libspf2/spf_server.c
+++ src/libspf2/spf_server.c
@@ -383,6 +383,10 @@ retry:
 			if (spf_server->debug > 0)
 				SPF_debugf("get_record(%s): TRY_AGAIN", domain);
 			SPF_dns_rr_free(rr_txt);
+			if (rr_type == ns_t_spf) {
+				rr_type = ns_t_txt;
+				goto retry;
+			}
 			spf_response->result = SPF_RESULT_TEMPERROR;
 			spf_response->reason = SPF_REASON_FAILURE;
 			return SPF_response_add_error(spf_response, SPF_E_DNS_ERROR,
@@ -393,6 +397,10 @@ retry:
 			if (spf_server->debug > 0)
 				SPF_debugf("get_record(%s): NO_RECOVERY", domain);
 			SPF_dns_rr_free(rr_txt);
+			if (rr_type == ns_t_spf) {
+				rr_type = ns_t_txt;
+				goto retry;
+			}
 			spf_response->result = SPF_RESULT_PERMERROR;
 			spf_response->reason = SPF_REASON_FAILURE;
 			return SPF_response_add_error(spf_response, SPF_E_DNS_ERROR,
@@ -409,6 +417,10 @@ retry:
 				SPF_debugf("get_record(%s): UNKNOWN_ERROR", domain);
 			herrno = rr_txt->herrno;	// Avoid use-after-free
 			SPF_dns_rr_free(rr_txt);
+			if (rr_type == ns_t_spf) {
+				rr_type = ns_t_txt;
+				goto retry;
+			}
 			return SPF_response_add_error(spf_response, SPF_E_DNS_ERROR,
 					"Unknown DNS failure for '%s': %d.",
 					domain, herrno);
